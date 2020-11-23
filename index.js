35ed9fb59d29e3342b85f9baefcd9dfcbb5bbb5a
Eze Sunday Eze <mailsforeze@gmail.com>
2020-11-23 10:17:41 +0100

    modify index file

--- index.js
+++ index.js
@@ -7,7 +7,4 @@ require("./src/middlewares/pre-route.middleware")(app);
 // API routes
 app.use("/api", require("./src/routes/index.route"));
 
-// Ping route for testing connection
-app.get("/ping", (req, res) => res.status(200).send("Hello world!"));
-
 module.exports = app;
