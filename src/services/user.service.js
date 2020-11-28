65a703c0ea7bac3d7a934c36301d23a514abcb8a
Eze Sunday Eze <mailsforeze@gmail.com>
2020-11-28 22:18:43 +0100

    refactor auth service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,40 +1,183 @@
+const JWT = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const User = require("./../models/user.model");
+const Token = require("./../models/token.model");
 const CustomError = require("./../utils/customError");
+const config = require("../config/env");
+const sendEmail = require("../utils/emailer/sendEmail");
 
-class UserService {
-  async create(data) {
-    return await new User(data).save();
+let env = process.env.NODE_ENV || "development";
+
+const JWTSecret = config[env].jwtSecret;
+const bcryptSalt = config[env].bcryptSalt;
+const url = config[env].url;
+
+class AuthService {
+  async signup(data) {
+    let user = await User.findOne({ email: data.email });
+    if (user) throw new CustomError("Email already exists");
+    user = new User(data);
+
+    const token = JWT.sign({ id: user._id }, JWTSecret);
+    await user.save();
+    await this.RequestEmailVerification(user.email);
+    return (data = {
+      uid: user._id,
+      email: user.email,
+      role: user.role,
+      token: token,
+    });
   }
 
-  async getAll() {
-    return await User.find({}, { password: 0, __v: 0 });
+  async signin(data) {
+    if (!data.email) throw new CustomError("Email is required");
+    if (!data.password) throw new CustomError("Password is required");
+
+    // Check if user exist
+    const user = await User.findOne({ email: data.email });
+    if (!user) throw new CustomError("Incorrect email or password");
+
+    //Check if user password is correct
+    const isCorrect = await bcrypt.compare(data.password, user.password);
+    if (!isCorrect) throw new CustomError("Incorrect email or password");
+    const token = await JWT.sign({ id: user._id, role: user.role }, JWTSecret, {
+      expiresIn: 60 * 60,
+    });
+
+    return (data = {
+      uid: user._id,
+      email: user.email,
+      role: user.role,
+      verified: user.isVerified,
+      token: token,
+    });
   }
 
-  async getOne(userId) {
-    const user = await User.findOne({ _id: userId }, { password: 0, __v: 0 });
-    if (!user) throw new CustomError("User does not exists");
+  async updatePassword(userId, data) {
+    const user = await User.findOne({ _id: userId });
+    if (!user) throw new CustomError("User dose not exist");
 
-    return user;
-  }
+    //Check if user password is correct
+    const isCorrect = await bcrypt.compare(data.password, user.password);
+    if (!isCorrect) throw new CustomError("Incorrect password");
 
-  async update(userId, data) {
-    const user = await User.findByIdAndUpdate(
+    const hash = await bcrypt.hash(password, Number(bcryptSalt));
+
+    await User.updateOne(
       { _id: userId },
-      { $set: data },
+      { $set: { password: hash } },
       { new: true }
     );
 
-    if (!user) throw new CustomError("User dosen't exist", 404);
-
-    return user;
+    return;
   }
 
-  async delete(userId) {
+  async RequestEmailVerification(email) {
+    const user = await User.findOne({ email });
+    if (!user) throw new CustomError("Email does not exist");
+    if (user.isVerified) throw new CustomError("Email is already verified");
+
+    let token = await Token.findOne({ userId: user._id });
+    if (token) await token.deleteOne();
+
+    let verifyToken = crypto.randomBytes(32).toString("hex");
+    const hash = await bcrypt.hash(verifyToken, Number(bcryptSalt));
+
+    await new Token({
+      userId: user._id,
+      token: hash,
+      createdAt: Date.now(),
+    }).save();
+
+    const link = `${url.client}/email-verification?uid=${user._id}&verifyToken=${verifyToken}`;
+
+    //send mail
+    sendEmail(
+      user.email,
+      "Raveshift: Verify Email",
+      {
+        name: user.name,
+        link,
+      },
+      "./template/emailVerify.handlebars"
+    );
+    return link;
+  }
+
+  async VerifyEmail(data) {
+    const { userId, verifyToken } = data;
+
     const user = await User.findOne({ _id: userId });
-    user.isActive = false;
-    user.save();
-    return user;
+    console.log(user);
+    if (!user) throw new CustomError("User does not exist");
+    if (user.isVerified) throw new CustomError("Email is already verified");
+
+    let VToken = await Token.findOne({ userId });
+
+    if (!VToken)
+      throw new CustomError("Invalid or expired password reset token");
+    const isValid = await bcrypt.compare(verifyToken, VToken.token);
+
+    if (!isValid)
+      throw new CustomError("Invalid or expired password reset token");
+
+    await User.updateOne(
+      { _id: userId },
+      { $set: { isVerified: true } },
+      { new: true }
+    );
+
+    await VToken.deleteOne();
+
+    return;
+  }
+
+  async RequestPasswordReset(email) {
+    const user = await User.findOne({ email });
+    if (!user) throw new CustomError("Email does not exist");
+
+    let token = await Token.findOne({ userId: user._id });
+    if (token) await token.deleteOne();
+
+    let resetToken = crypto.randomBytes(32).toString("hex");
+    const hash = await bcrypt.hash(resetToken, Number(bcryptSalt));
+
+    await new Token({
+      userId: user._id,
+      token: hash,
+      createdAt: Date.now(),
+    }).save();
+
+    const link = `${url.CLIENT_URL}/reset-password?uid=${user._id}&resetToken=${resetToken}`;
+
+    // send email
+    return link;
+  }
+
+  async resetPassword(data) {
+    const { userId, resetToken, password } = data;
+
+    let RToken = await Token.findOne({ userId });
+    if (!RToken)
+      throw new CustomError("Invalid or expired password reset token");
+
+    const isValid = await bcrypt.compare(resetToken, RToken.token);
+    if (!isValid)
+      throw new CustomError("Invalid or expired password reset token");
+
+    const hash = await bcrypt.hash(password, Number(bcryptSalt));
+
+    await User.updateOne(
+      { _id: userId },
+      { $set: { password: hash } },
+      { new: true }
+    );
+
+    await RToken.deleteOne();
+
+    return;
   }
 }
 
-module.exports = new UserService();
+module.exports = new AuthService();
