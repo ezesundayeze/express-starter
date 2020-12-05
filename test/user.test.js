53ee64376c0873fcb6105bb337a74c52c549a7cc
Eze Sunday Eze <mailsforeze@gmail.com>
2020-12-05 12:42:09 +0100

    add test to user login

--- test/user.test.js
+++ test/user.test.js
@@ -11,11 +11,19 @@ afterAll(async () => {
 describe("User Test", () => {
   it("should create a new user", async () => {
     const user = await request.post(`${TEST_BASE_URL}/auth/signup`).send({
-      email: "mailstoeze@gmail.com",
+      email: "mail6@gmail.com",
       name: "Peter",
       password: "Password",
     });
     expect(user.body.error).toBe(false);
-    expect(user.body.data.email).toBe("mailstoeze@gmail.com");
+    expect(user.body.data.email).toBe("mail6@gmail.com");
+  });
+  it("should log a user in", async () => {
+    const user = await request.post(`${TEST_BASE_URL}/auth/signin`).send({
+      email: "mail6@gmail.com",
+      password: "Password",
+    });
+    expect(user.body.error).toBe(false);
+    expect(user.body.email).toBe("mail6@gmail.com");
   });
 });
